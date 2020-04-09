fca2688acbcf741c711b9b398728f365ae81d15a
nsc-sugino <63280618+nsc-sugino@users.noreply.github.com>
2020-04-10 08:03:29 +0900

    js

---
+++ html/js/factorial.js
@@ -0,0 +1,9 @@
+
+ var random = 1 + Math.floor(Math.random() * 10);
+
+ for (var index = random; index > 0; index--) {
+    var result = 1;
+    result = result * index;
+ }
+ console.log(result);
+ 
