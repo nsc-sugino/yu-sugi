bb197693e72f1a5a833a8879032f5c594b50f63a
nsc-sugino <63280618+nsc-sugino@users.noreply.github.com>
2020-04-10 07:32:20 +0900

    html + js

---
+++ html/js/area.js
@@ -0,0 +1,12 @@
+function getArea(length,width) {
+    let area = (length * width) ;
+    return area;
+}
+console.log(getArea());
+
+function getPerimeter(length,width) {
+    let perimeter = (length + width + length + width);
+
+    return perimeter;
+}
+console.log(getPerimeter());
