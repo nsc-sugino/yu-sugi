9b60408f4b42581199d2da8da3ff7e8ce00a8926
nsc-sugino <63280618+nsc-sugino@users.noreply.github.com>
2020-04-10 10:55:55 +0900

    js

--- html/js/area.js
+++ html/js/area.js
@@ -1,12 +1,18 @@
+let min = 1;
+let max = 1000;
+let number1 = Math.floor( Math.random() * (max + 1 - min) ) + min ;
+let number2 = Math.floor( Math.random() * (max + 1 - min) ) + min ;
+let length = number1;
+let width = number2;
+
 function getArea(length,width) {
-    let area = (length * width) ;
+    var area = length * width;
     return area;
 }
-console.log(getArea());
+console.log('面積は' + getArea() );
 
 function getPerimeter(length,width) {
-    let perimeter = (length + width + length + width);
-
+    var perimeter = (length + width) * 2;
     return perimeter;
 }
-console.log(getPerimeter());
+console.log('周囲の長さは' + getPerimeter());
--- html/js/factorial.js
+++ html/js/factorial.js
@@ -1,7 +1,12 @@
-let random = 1 + Math.floor(Math.random() * 10);
+let min = 1 ;
+let max = 10 ;
 
- for (let index = random; index > 0; index--) {
-    var result = 1;
+let random = Math.floor( Math.random() * (max + 1 - min) ) + min ;
+
+let result = 1;
+
+for (let index = random; index > 0; index--) {
     result = result * index;
  }
+
  console.log(result);
